0d15a97de5a4923e364b7f078d24ac0e0a39eb96
Gongbu22 <99306831+gongbu22@users.noreply.github.com>
2024-09-11 13:56:15 +0900

    terraform AWS EC2 instance - ubuntu

---
+++ ec2_instance_ubuntu/variables.tf
@@ -0,0 +1,29 @@
+variable "ami" {
+  description = "fastapi ami"
+  type = string
+}
+
+variable "instance_type" {
+  description = "instance_type"
+  type = string
+}
+
+variable "key_name" {
+  description = "key name"
+  type = string
+}
+
+variable "security_group_id" {
+  description = "fastapi security group id"
+  type = string
+}
+
+variable "instance_name" {
+  description = "fastapi instance name"
+  type = string
+}
+
+variable "user_data" {
+  description = "user script to execute"
+  type = string
+}
