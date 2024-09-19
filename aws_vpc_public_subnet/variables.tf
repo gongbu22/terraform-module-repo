944295ccbc72935069b3cf1587383dbaf1854003
Gongbu22 <99306831+gongbu22@users.noreply.github.com>
2024-09-19 14:05:40 +0900

    terraform AWS VPC public subnet

---
+++ aws_vpc_public_subnet/variables.tf
@@ -0,0 +1,25 @@
+variable "instance_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  description = "vpc cidr"
+  type = string
+}
+
+variable "subnet_cidr" {
+  type = string
+}
+
+variable "avail_zone" {
+  description = "vpc availability_zone"
+  type = string
+}
+
+variable "inbound_ports" {
+  type = list(object({
+    port = number
+    protocol = optional(string, "tcp")  # optional : terraform 1.3.0 이상
+    cidr_blocks = optional(list(string), ["0.0.0.0/0"])
+  }))
+}
