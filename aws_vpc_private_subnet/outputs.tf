296e2607a99fe9da1134a71b576c0f559dc58e57
Gongbu22 <99306831+gongbu22@users.noreply.github.com>
2024-09-20 10:50:03 +0900

    수정된 terraform AWS VPC private subnet

---
+++ aws_vpc_private_subnet/outputs.tf
@@ -0,0 +1,27 @@
+output "private_subnet_ids" {
+  value = {
+    for key, subnet in aws_subnet.private_subnet: key => subnet.id
+  }
+}
+
+output "private_sg_ids" {
+  value = {
+    for key, sg in aws_security_group.private_sg: key => sg.id
+  }
+}
+
+# output "private_sg_fastapi_id" {
+#   value = aws_security_group.private_sg["fastapi"].id
+# }
+#
+# output "private_sg_mariadb_id" {
+#   value = aws_security_group.private_sg["mariadb"].id
+# }
+#
+# output "private_subnet_fastapi_id" {
+#   value = aws_subnet.private_subnet["fastapi"].id
+# }
+#
+# output "private_subnet_mariadb_id" {
+#   value = aws_subnet.private_subnet["mariadb"].id
+# }
